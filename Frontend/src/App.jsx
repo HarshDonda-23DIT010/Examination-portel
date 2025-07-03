53077f7efbdec7bf40958561b070b5770673a35d
Ansh Singh <ansh28352@gmail.com>
2025-07-03 14:58:24 +0530

    refactor: remove unused imports and reset greeting in App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <div className='text-7xl flex items-center justify-center h-screen'>
-    <h1>Good One</h1>
+    <h1>Hello</h1>
    </div>
   )
 }
