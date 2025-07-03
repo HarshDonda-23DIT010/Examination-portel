e409b30f5d781569a16f657bf0375b6da58f00cd
sasparavinit@gmail.com <sasparavinit@gmail.com>
2025-07-03 16:00:22 +0530

    refactor: streamline database connection and improve code formatting

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,12 +1,9 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
-dotenv.config();
 
-const URL = process.env.MONGO_URL;
 
 export const connectionDb = async () => {
     try {
-        await mongoose.connect(URL);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log(`MONGODB CONNECTED`);
     } catch (err) {
         console.log("Mongo Connection failed:", err.message);
--- Backend/index.js
+++ Backend/index.js
@@ -1,35 +1,36 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import dotenv from 'dotenv'
 import { connectionDb } from './db/db.js'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = express()
 
 
 // Middlewares
 app.use(cors({
-    origin : '*',
-    methods : ['GET','POST','PUT','DELETE','PATCH'] ,
-    credentials:true,
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        credentials: true,
 }))
 
 app.use(express.json())
-app.use(express.urlencoded({extended :true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 // routes
 
 
 // server
-const port  = process.env.PORT
-app.listen(port , ()=>{
+const port = process.env.PORT
+app.listen(port, () => {
         connectionDb();
         console.log(`Server running at port :${port}`);
 })
 
-app.get('/' , (req,res) => {
+app.get('/', (req, res) => {
         res.send("<h1>HELLO WORLD</h1>")
 })
 
